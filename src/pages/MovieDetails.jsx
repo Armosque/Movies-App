45700fd9f8892fe825da0b7ee6f09a6a3a9802b5
armosque <armosqueral@yahoo.es>
2023-03-01 21:10:22 -0500

    'app-json'

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,17 +1,25 @@
 import './MovieDetails.css';
-import movies from '../movies.json'
+import {useParams} from 'react-router-dom';
+import movie from '../movies.json'
 
 export function MovieDetails() {
+    
+    const {movieId} = useParams();
+    console.log(movie)
+    const pelis = movie.find((e)=>e.id===Number(movieId));
+  
     
     return (
         
         <div className='detailsContainer'>
-        {
-            movies.map((movie)=>( 
-                <img key={movie.id} src={'http://image.tmdb.org/t/p/w500'+ movie.poster_path} alt={movie.title} />
-            ))
-        }
-    
+        
+        <img className='col movieImage' src={`http://image.tmdb.org/t/p/w300${pelis.poster_path}`} alt="" />
+        <div className='col movieDetails'>
+     <p><strong className='encabezado'>Title:</strong> {pelis.original_title}</p>
+     <p><strong className='encabezado'>Description:</strong> {pelis.overview}</p>
+     <p><strong className='encabezado'>Score:</strong> {pelis.vote_average}</p>
+     {/* <p><strong>{movie.genres.map(genre=>genre.name).join(', ')}</strong></p> */}
+     </div>
         </div>
     )
 }
